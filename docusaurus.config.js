fc018b7c3b194d6032db34c0f636ba57fade3c5b
Eakampreet <eakam1007@gmail.com>
2022-10-28 21:00:21 -0400

    Update base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Sample Site',
   tagline: 'Dinosaurs are cool and so are you',
   url: 'https://eakam1007.github.io/docusaurus-sample',
-  baseUrl: '/',
+  baseUrl: '/docusaurus-sample/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
